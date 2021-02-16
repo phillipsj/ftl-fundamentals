04013e873534214a531383bd7e886f00645833dc
Jamie Phillips <cphill11@gmail.com>
2021-02-15 22:38:59 -0500

    Through chapter 7

--- calculator.go
+++ calculator.go
@@ -1,6 +1,8 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import "fmt"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -15,4 +17,11 @@ func Subtract(a, b float64) float64 {
 // Multiply takes two numbers and returns the result.
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("bad inputs %f, %f: division by zero is not allowed", a, b)
+	}
+	return a / b, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -6,8 +6,9 @@ import (
 )
 
 type testCase struct {
-	a, b float64
-	want float64
+	a, b        float64
+	want        float64
+	errExpected bool
 }
 
 func TestAdd(t *testing.T) {
@@ -54,6 +55,29 @@ func TestMultiply(t *testing.T) {
 		got := calculator.Multiply(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 2, b: 1, want: 2},
+		{a: 4, b: 2, want: 2},
+		{a: 4, b: 0, want: 0, errExpected: true},
+	}
+
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+
+		errReceived := err != nil
+		if tc.errExpected != errReceived {
+			t.Fatalf("Divide(%f, %f): unexpected error status: %v", tc.a, tc.b, errReceived)
+		}
+
+		if !tc.errExpected && tc.want != got {
+			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
 		}
 	}
 }
